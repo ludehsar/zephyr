1b612ed2f781e43f5c0647b73e4367108f777e88
Rashedul Alam <rashedulalam112112@gmail.com>
2025-04-04 17:04:24 +0600

    feature: setup sst monorepo

--- sst.config.ts
+++ sst.config.ts
@@ -1,18 +1,22 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "zephyr",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        aws: {
+          profile: process.env.AWS_PROFILE,
+        },
+        stripe: "0.0.24",
+      },
     };
   },
   async run() {
     const storage = await import("./infra/storage");
     await import("./infra/api");
-
     return {
       MyBucket: storage.bucket.name,
     };
